10e28f78d4b6b68261904f0f41426219229b85f0
David Santana <david@2coders.com>
2021-08-14 19:05:38 +0100

    adding output

--- obtain-version.sh
+++ obtain-version.sh
@@ -8,11 +8,11 @@
 # obtain-version.sh MyApp/Info.plist
 
 if [ -z "$1" ];
-then echo "Info.plist path cant be null or empty";
+then echo "::error::Info.plist path cant be null or empty";
 else 
     plist_version=$(/usr/libexec/PlistBuddy -c "Print XCFrameworkFormatVersion" $1)
     if [ -z "$plist_version" ]; 
-     then echo "Can't find version";
+     then echo "::error::Can't find version";
     else
         echo "::debug::Found version - $1 version: $plist_version"
         echo "::set-output name=version::4.0"
