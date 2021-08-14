4e69244bf4fc3ebfb7a68f3cb2dcde665b46fe37
David Santana <david@2coders.com>
2021-08-14 19:35:42 +0100

    adding variants for XCFrameworkFormatVersion and CFBundleShortVersionString as keys

--- obtain-version.sh
+++ obtain-version.sh
@@ -11,21 +11,21 @@ echo "Entry data:"
 echo "Plist path: $1"
 echo "Key: $2"
 
-if [ -z "$1" ];
+if [[ -z "$1" || -z "$2" ]];
 then echo "::error::Info.plist path cant be null or empty";
 else 
     
-    if [$2 == "XCFrameworkFormatVersion"] || [$2 == "CFBundleShortVersionString" ];
+    if [[$2 == "XCFrameworkFormatVersion" || $2 == "CFBundleShortVersionString" ]]
         then
         plist_version=$(/usr/libexec/PlistBuddy -c "Print $2" $1)
         if [ -z "$plist_version" ]; 
-            then echo "::error Can't find version";
+            then echo "::error Can't find version"
         else
             echo "::debug::Found version - $1 version: $plist_version"
             echo "::set-output name=version::$plist_version"
         fi
     else
-        then echo "::error No valid keys";
+        then echo "::error::No valid keys"
     fi
     
 fi
