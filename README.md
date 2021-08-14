6360e7081ccd6ee29a334bf104847c3cfe1a46d5
David Santana <david@2coders.com>
2021-08-14 17:51:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Obtain-version
+Obtain the version in the plist file
+
+Use in workflow as follows:
+
+        - uses: 2CodersStudio/ActionPlistVersion@main
+        id: plistVersion
+        with:
+          plist-path: PathToInfoPlistFolder/Info.plist
+        - name: Get Version
+        run: echo Version ${{ steps.plistVersion.outputs.version }}"
---
+++ obtain-version.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Retrive the plist version
+# $1 - path to Info.plist file containing CFBundleShortVersionString
+#
+
+# Example:
+# obtain-version.sh MyApp/Info.plist
+
+plist_version=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" $1)
+echo "Info.plist version: $plist_version"
+echo "::set-output name=version::$plist_version"
