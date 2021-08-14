b6272d89c206d89f608e7cd14d8eedd528ab5613
David Santana <david@2coders.com>
2021-08-14 18:08:40 +0100

    change key for XCFrameworkFormatVersion

--- obtain-version.sh
+++ obtain-version.sh
@@ -7,6 +7,6 @@
 # Example:
 # obtain-version.sh MyApp/Info.plist
 
-plist_version=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" $1)
+plist_version=$(/usr/libexec/PlistBuddy -c "Print XCFrameworkFormatVersion" $1)
 echo "Info.plist version: $plist_version"
 echo "::set-output name=version::$plist_version"
