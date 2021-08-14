258b3af012658cfd0a63fc790084b68bf17cb40f
David Santana <david@2coders.com>
2021-08-14 18:35:13 +0100

    change key for script logic

--- obtain-version.sh
+++ obtain-version.sh
@@ -7,6 +7,17 @@
 # Example:
 # obtain-version.sh MyApp/Info.plist
 
-plist_version=$(/usr/libexec/PlistBuddy -c "Print XCFrameworkFormatVersion" $1)
-echo "Info.plist version: $plist_version"
-echo "::set-output name=version::$plist_version"
+if [ -z "$1" ];
+then echo "Info.plist path cant be null or empty";
+else 
+    plist_version=$(/usr/libexec/PlistBuddy -c "Print XCFrameworkFormatVersion" $1)
+    if [ -z "$plist_version" ]; 
+     then echo "Can't find version";
+    else
+        echo "::debug::Found version - $1 version: $plist_version"
+        echo "::set-output name=version::4.0"
+    fi
+    
+fi
+
+
